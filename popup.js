3ddd29d34cf4072ae81602b1b43758c8ac3aaae9
Khanh Duc Tran <ktran035@uw.edu>
2017-01-28 13:12:08 -0800

    what

--- popup.js
+++ popup.js
@@ -1,13 +1,26 @@
 
 window.onload = function() {
 	setVisible();
-	console.log("This is location" + location);
+	// console.log("This is location" + location);
 }
+
 
 
 function setVisible() {
 	chrome.runtime.sendMessage ( {command: "gimmeGimme"}, function (response) {
-	    console.log(response.geoLocation);
-	    console.log("123");
+		var location = response.geoLocation;
+		var loc = location.split(",");
+        $.post( "https://maps.googleapis.com/maps/api/geocode/json?latlng="+loc[0]+"," + loc[1]+ "&key=AIzaSyCngncdCEqVb_fy5xpIs1MTxSaYn9sszkc", function( data ) {
+            // $( ".result" ).html( data );
+            var address = data.results[0]["formatted_address"].split(",");
+            findMissingChild(address);
+        });
+	    // console.log(response.geoLocation);
+	    // console.log("123");
 	} );
 }
+
+function findMissingChild(address) {
+
+}
+
