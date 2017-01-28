e1a488c69f6efc189997de12ae0442d7c000a887
Quyen Truong <quyent22@cs.washington.edu>
2017-01-28 12:27:50 -0800

    add location

--- popup.js
+++ popup.js
@@ -2,14 +2,19 @@
 
 window.onload = function() {
 	setVisible();
-	console.log("This is location" + location);
+	// console.log("This is location" + location);
 }
+
 
 
 function setVisible() {
 	chrome.runtime.sendMessage ( {command: "gimmeGimme"}, function (response) {
-	    console.log(response.geoLocation);
-	    console.log("123");
+        $.post( "https://maps.googleapis.com/maps/api/geocode/json?latlng=47.6,-122.30511159999999&key=AIzaSyCngncdCEqVb_fy5xpIs1MTxSaYn9sszkc", function( data ) {
+            // $( ".result" ).html( data );
+            console.log(data.results[0]["formatted_address"])
+        });
+	    // console.log(response.geoLocation);
+	    // console.log("123");
 	} );
 }
 
